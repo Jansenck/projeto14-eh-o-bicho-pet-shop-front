5d475088fc0f1041882771506dcdc6021df7d392
Tissyane <tissyane@gmail.com>
2022-09-14 15:11:03 -0300

    Creating Global Styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
-export default function App(){
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<p>Teste</p>} ></Route>
-            </Routes>
-        </BrowserRouter>
-    );
+import GlobalStyle from "../styles/GlobalStyles";
+
+const theme = {
+  white: "#FFFFFF",
+  black: "#000000",
+  darkblue: "#05668d",
+  lightblue: "#028090",
+  darkgreen: "#00a896",
+  lightgreen: "#02c39a",
+  lightyellow: "#f0f3bd",
+};
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
