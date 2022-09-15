60f2ed95ba1bccb758dc35afbc234042b79da9f6
guedesclaudio <cgsarmentosilva@gmail.com>
2022-09-15 10:35:03 -0300

    created sub top

--- src/components/Top.js
+++ src/components/Top.js
@@ -3,23 +3,40 @@ import styled from "styled-components";
 import { FaBeer } from 'react-icons/fa';
 //import { IoPersonOutline } from 'react-icons/fa';
 import { Link } from "react-router-dom"
+import { useState } from "react";
 
 export default function Top () {
 
+    const [display, setDisplay] = useState("none")
+
+    function showMenu() {
+        display === "none" ? setDisplay("flex") : setDisplay("none")
+    }
+
     return (
-        <Container>
-            <LogoText>
-                É o bicho!
-            </LogoText>
-            <Icons>
-                <Link to = {"/cart"}>
-                    <FaBeer color = {"#FFFFFF"} fontSize = {25}/>
-                </Link>
-                <Link to = {"/signin"}>
-                    <FaBeer color = {"#FFFFFF"} fontSize = {25}/>
-                </Link>
-            </Icons>
-        </Container>
+        <>
+            <Container>
+                <Menu onClick = {showMenu}>
+
+                </Menu>
+                <LogoText>
+                    É o bicho!
+                </LogoText>
+                <Icons>
+                    <Link to = {"/cart"}>
+                        <FaBeer color = {"#FFFFFF"} fontSize = {25}/>
+                    </Link>
+                    <Link to = {"/signin"}>
+                        <FaBeer color = {"#FFFFFF"} fontSize = {25}/>
+                    </Link>
+                </Icons>
+            </Container>
+            <SubTop show = {display}>
+                <p>Cães</p>
+                <p>Gatos</p>
+                <p>Coelhos</p>
+            </SubTop>
+        </>
     )
 }
 
@@ -37,14 +54,17 @@ const Container = styled.div`
     z-index: 10;
     box-shadow: 1px 1px 1px 1px rgba(0,0,0,0.3);
 `
-
+const Menu = styled.div`
+    width: 40px;
+    height: 40px;
+    border: 1px solid white;
+    margin-left: 10px;
+`
 const LogoText = styled.h1`
     font-size: 30px;
     color: #FFFFFF;
     font-family: Arial, Helvetica, sans-serif;
-    margin-left: 10px;
 `
-
 const Icons = styled.div`
     width: 80px;
     height: 100%;
@@ -52,4 +72,16 @@ const Icons = styled.div`
     justify-content: space-between;
     align-items: center;
     margin-right: 10px;
+`
+const SubTop = styled.div`
+    width: 100vw;
+    margin-top: 80px;
+    height: 40px;
+    background-color: #02c39a;
+    display: ${props => props.show};
+    justify-content: space-around;
+    align-items: center;
+    color: white;
+    font-family: Arial, Helvetica, sans-serif;
+    opacity: 0.5;
 `
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,35 +6,16 @@ export default function HomePage () {
     return (
         <>
             <Top/>
-            <Container>
-                <Cover src = "https://t4.ftcdn.net/jpg/01/81/62/07/360_F_181620791_9W5pfwlMLajAavp7MnJOoEANziwbxc5w.jpg"/>
-                <CoverText>Tudo o que seu pet precisa!</CoverText>
-            </Container>
             
+            <Container>
+                {/*<Cover src = "https://t4.ftcdn.net/jpg/01/81/62/07/360_F_181620791_9W5pfwlMLajAavp7MnJOoEANziwbxc5w.jpg"/>*/}
+            </Container>
         </>
     )
 }
 
 const Container = styled.div`
-    margin-top: 80px;
 `
 const Cover = styled.img`
     width: 100vw;
-    position: relative;
-`
-const CoverText = styled.h1`
-    position: absolute;
-    width: 300px;
-    font-size: 20px;
-    height: 40px;
-    top: 180px;
-    left: 28px;
-    color: #05668d;
-    font-family: Arial, Helvetica, sans-serif;
-    font-weight: bold;
-    background-color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 2px solid #05668d;
 `
