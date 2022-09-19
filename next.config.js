6328eb0130569a548a3a684721fa1cbf837fb582
Tissyane <tissyane@gmail.com>
2022-09-19 16:13:53 -0300

    Changing source nextconfig

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: "/api/(.*)",
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
